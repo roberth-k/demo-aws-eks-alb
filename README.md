14d06f3414227a1a67d57bd2a3f4e4aa9f349419
Rob Kulbin <6707630+roberth-k@users.noreply.github.com>
2023-03-14 17:48:22 +0000

    create minimal VPC

---
+++ README.md
@@ -0,0 +1,4 @@
+Minimal demo: AWS EKS with ALB
+==============================
+
+- Uses the default backend configuration (local file named `terraform.tfstate` -- **this has been .gitignore-d!**).
---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.58"
+    }
+  }
+}
+
+provider "aws" {}
+
+data "aws_region" "main" {}
+
+locals {
+  region = data.aws_region.main.name
+}
+
+// https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/3.19.0
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "demo"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+}
